92d67e98e3f464eb1f9cf2419db6465f013b51bc
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-28 20:49:40 -0300

    Adicionando Controller(TextEditingController) para pegar o valor digitado nos TextFields, e os metodos para calcular o IMC

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //pega o que eu digito dentro dos TextFields
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _info = "Informe seus dados";
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      _info = "Informe seus dados";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+
+      double imc = weight / (height * height);
+
+      print(imc);
+      if (imc < 18.6) {
+        _info = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _info = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _info = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _info = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _info = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc > 40.0) {
+        _info = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +61,9 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: () {
+              _resetFields();
+            },
           )
         ],
       ),
@@ -46,6 +86,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25),
+              controller: weightController,
             ),
             SizedBox(
               height: 30,
@@ -58,13 +99,17 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25),
+              //Preciso colocar para informar qual o controller da minha TextField
+              controller: heightController,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
               child: Container(
                 height: 50.0,
                 child: RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _calculate();
+                  },
                   color: Colors.green,
                   child: Text(
                     "Calcular",
@@ -74,7 +119,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              "Info",
+              "$_info",
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25),
             )
